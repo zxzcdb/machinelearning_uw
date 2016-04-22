8ebf82441e82f0e041e8dc91fb95e97f4cd4dd2c
klhscottxzz <tianxiashilaofude@gmail.com>
2016-04-22 19:46:27 +0800

    Delete slregression.py

--- week1/slregression.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-#source ~/ "test27" /bin/activate
-
-with open('kc_house_train_data.csv','rb') as csvfile:
-        cdata = csv.reader(csvfile)
-        for row in cdata:
-            print ', '.join(row)
