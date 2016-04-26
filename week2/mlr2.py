305ad34a76f045e6d659645d3c20f13c53145461
klhscottxzz <tianxiashilaofude@gmail.com>
2016-04-26 15:46:29 +0800

    Update mlr2.py

--- week2/mlr2.py
+++ week2/mlr2.py
@@ -1,6 +1,6 @@
 import graphlab as gl
-import graphlab.numpy
 import numpy as np
+import sqrt from math
 
 sales = gl.SFrame('kc_house_data.gl/')
 
@@ -37,3 +37,72 @@ def predict_output(fm, wei):
 test_predictions = predict_output(example_features, my_weights)
 print test_predictions[0] # should be 1181.0
 print test_predictions[1] # should be 2571.0
+
+def feature_derivative(errors, feature):
+    dev = np.dot(errors, feature)
+    derivative = 2*dev
+    return(derivative)
+
+def regression_gradient_descent(feature_matrix, output, initial_weights, step_size, tolerance):
+    converged = False
+    weights = np.array(initial_weights)
+    while not converged:
+        initial_prediction = predict_output(feature_matrix,initial_weights)
+
+        errors = initial_prediction - output
+
+        gradient_sum_squares = 0
+
+        for i in range(len(weights)):
+            par_der = feature_derivative(errors,feature_matrix[:, i])
+
+            gradient = gradient + par_der * par_der
+
+            # update the weight based on step size and derivative:
+
+        gradient_magnitude = sqrt(gradient_sum_squares)
+        if gradient_magnitude < tolerance:
+            converged = True
+    return(weights)
+
+# train and test
+#Q1 & Q2
+train_data,test_data = sales.random_split(.8,seed=0)
+
+simple_features = ['sqft_living']
+my_output= 'price'
+(simple_feature_matrix, output) = get_numpy_data(train_data, simple_features, my_output)
+initial_weights = np.array([-47000., 1.])
+step_size = 7e-12
+tolerance = 2.5e7
+
+simple_weights = regression_gradient_descent(simple_feature_matrix, output,initial_weights, step_size,tolerance)
+
+predict1 = predict_output(simple_feature_matrix,simple_weights)
+
+#Q1
+print "What is the value of the weight for sqft_living -- the second element of ‘simple_weights’ ", simple_weights
+
+#Q2
+print "What is the predicted price for the 1st house in the Test data set for model 1", predict1
+
+#Q3, Q4 & Q5
+model_features = ['sqft_living', 'sqft_living15']
+my_output = 'price'
+(feature_matrix, output) = get_numpy_data(train_data, model_features,my_output)
+initial_weights = np.array([-100000., 1., 1.])
+step_size = 4e-12
+tolerance = 1e9
+
+weights2 = regression_gradient_descent(model_features, output,initial_weights, step_size,tolerance)
+
+predict2 = predict_output(model_features,weights2)
+
+#Q3
+print "What is the predicted price for the 1st house in the TEST data set for model 2 (round to nearest dollar)?"
+
+#Q4
+print "Which estimate was closer to the true price for the 1st house on the TEST data set, model 1 or model 2?"
+
+#Q5
+print "Quiz Question: Which model (1 or 2) has lowest RSS on all of the TEST data? "
