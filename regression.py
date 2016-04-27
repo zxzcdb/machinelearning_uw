aadd3e5b613d48695b2ab205ac3191c6d9de13c4
Xingzhou Zhu <tianxiashilaofude@gmail.com>
2016-04-27 23:46:52 +0800

    Create regression.py

---
+++ regression.py
@@ -0,0 +1,90 @@
+import graphlab as gl
+import numpy as np
+from math import sqrt
+from math import log
+
+# Week1: Simple Linear Regression
+def simple_linear_regression(input, output):
+    num = input.size()
+
+    in_sum = input.sum()
+    out_sum = output.sum()
+
+    pro = input * output
+    pro_sum = pro.sum()
+
+    in_sq = input * input
+    in_sq_sum = in_sq.sum()
+
+    avi = input.mean()
+    avo = output.mean()
+
+    # use the formula for the slope
+    slope = (pro_sum - num*avi*avo)/(in_sq_sum-num*avi*avi)
+
+    # use the formula for the intercept
+    intercept = avo - avi*slope
+
+    return (intercept, slope)
+
+# Week2: Multiple Linear Regression: Assignment1
+def get_residual_sum_of_squares(model, data, outcome):
+
+    prediction = model.predict(data)
+
+    residuals = prediction - outcome
+    RSS = residuals*residuals
+    RSS = RSS.sum()
+
+    return(RSS)
+
+# Week2: Multiple Linear Regression: Assignment2 Gradient Decent
+def get_numpy_data(data, features, output):
+
+    data['constant'] = 1
+
+    features = ['constant'] + features
+    ft = data.select_columns(features)
+    ftmatrix = ft.to_numpy()
+
+    outarray = np.asarray(data[output])
+
+    return(ftmatrix, outarray)
+
+def predict_output(fm, wei):
+    predictions = np.dot(fm, wei)
+    return(predictions)
+
+def feature_derivative(errors, feature):
+    der = np.dot(feature, errors)*2
+    return(der)
+
+def regression_gradient_descent(feature_matrix, output, initial_weights, step_size, tolerance):
+    converged = False
+    weights = np.array(initial_weights)
+    while not converged:
+        initial_prediction = predict_output(feature_matrix,initial_weights)
+        errors = initial_prediction - output
+        gradient_sum_squares = 0
+
+        for i in range(len(weights)):
+            deri = feature_derivative(errors,feature_matrix[:, i])
+            gradient_sum_squares = gradient_sum_squares + (deri * deri)
+            weights[i] = weights[i] - (step_size * deri)
+        gradient_magnitude = sqrt(gradient_sum_squares)
+        if gradient_magnitude < tolerance:
+            converged = True
+    return(weights)
+
+# Week 3: Performance Assessment
+def polynomial_sframe(feature, degree):
+    if degree < 1:
+        print "ERROR: degree < 1."
+    poly_sframe = gl.SFrame()
+    poly_sframe['power_1'] = feature
+    if degree > 1:
+        for power in range(2, degree+1):
+
+            name = 'power_' + str(power)
+            poly_sframe[name] = feature.apply(lambda x: x**power)
+    return poly_sframe
