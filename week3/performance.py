a26ae7574c2352c7fa3a5af4b895e9c79859b8e7
Xingzhou Zhu <tianxiashilaofude@gmail.com>
2016-05-02 15:48:58 +0800

    Update and rename polynomial_regression.py to performance.py

--- week3/performance.py
+++ week3/performance.py
@@ -2,66 +2,60 @@
 #%matplotlib inline
 
 import graphlab
-
 import matplotlib as mpl
 mpl.use('TkAgg')
-
 import matplotlib.pyplot as plt
-
-def polynomial_sframe(feature, degree):
-    # assume that degree >= 1
-    if degree < 1:
-        print "ERROR: degree < 1."
-    # initialize the SFrame:
-    poly_sframe = graphlab.SFrame()
-    # and set poly_sframe['power_1'] equal to the passed feature
-    poly_sframe['power_1'] = feature
-    # first check if degree > 1
-    if degree > 1:
-    # then loop over the remaining degrees:
-    # range usually starts at 0 and stops at the endpoint-1. We want it to start at 2 and stop at degree
-        for power in range(2, degree+1):
-            # first we'll give the column a name:
-            name = 'power_' + str(power)
-            # then assign poly_sframe[name] to the appropriate power of feature
-            poly_sframe[name] = feature.apply(lambda x: x**power)
-    return poly_sframe
+from regression import polynomial_sframe
+from regression import polynomial_fit
+from regression import get_residual_sum_of_squares as rss
 
 sales = graphlab.SFrame('kc_house_data.gl/')
 
 sales = sales.sort(['sqft_living', 'price'])
 
-# Model 1
+# Model 1: 1 feature & degree 1
 poly1_data = polynomial_sframe(sales['sqft_living'], 1)
+
 poly1_data['price'] = sales['price'] # add price to the data since it's the target
 
 model1 = graphlab.linear_regression.create(poly1_data, target = 'price', features = ['power_1'], validation_set = None)
 
 #let's take a look at the weights before we plot
+print "Coefficients of model with 1 degree:"
+
 model1.get("coefficients")
 
 plt.plot(poly1_data['power_1'],poly1_data['price'],'.',
         poly1_data['power_1'], model1.predict(poly1_data),'-')
 
-# Model 2
+# Model 2: 1 feature & degree 2
 poly2_data = polynomial_sframe(sales['sqft_living'], 2)
+
 my_features = poly2_data.column_names() # get the name of the features
+
 poly2_data['price'] = sales['price'] # add price to the data since it's the target
+
 model2 = graphlab.linear_regression.create(poly2_data, target = 'price', features = my_features, validation_set = None)
+
+print "Coefficients of model with 2 degree:"
 
 model2.get("coefficients")
 
 plt.plot(poly2_data['power_1'],poly2_data['price'],'.',
         poly2_data['power_1'], model2.predict(poly2_data),'-')
 
-# Model 3
+# Model 3:  1 feature & degree 3
 poly3_data = polynomial_sframe(sales['sqft_living'], 3)
 
 my_features3 = poly3_data.column_names() # get the name of the features
-print my_features3
+
+print "Features of model3 (degree 3) is: ", my_features3
+
 poly3_data['price'] = sales['price'] # add price to the data since it's the target
+
 model3 = graphlab.linear_regression.create(poly3_data, target = 'price', features = my_features3, validation_set = None)
 
+print "Coefficients of model with 1 degree:"
 model3.get("coefficients")
 
 plt.plot(poly3_data['power_1'],poly3_data['price'],'.',
@@ -73,17 +67,6 @@ plt.plot(poly3_data['power_1'],poly3_data['price'],'.',
 sub_1_1, sub_1_2 = sub_1.random_split(0.5, seed=0)
 sub_2_1, sub_2_2 = sub_2.random_split(0.5, seed=0)
 
-def polynomial_fit(data, degree):
-    poldata = polynomial_sframe(data['sqft_living'],degree)
-    feature = poldata.column_names()
-
-    poldata['price'] = data['price']
-
-    model = graphlab.linear_regression.create(poldata, target = 'price', features = feature, validation_set = None)
-    model.get("coefficients")
-
-    plt.plot(poldata['power_1'],polydata['price'],'.',poldata['power_1'], model.predict(poldata),'-')
-
 polynomial_fit(sub_1_1, 15)
 
 polynomial_fit(sub_1_2, 15)
@@ -92,30 +75,26 @@ polynomial_fit(sub_2_1, 15)
 
 polynomial_fit(sub_2_2, 15)
 
-# Quiz Question: Is the sign (positive or negative) for power_15 the same in all four models?
-# No
-
-# Quiz Question: True/False the plotted fitted lines look the same in all four plots
-# F
-
 #Selecting a Polynomial Degree
 train_validation_data, test_data =sales.random_split(0.9, seed=1)
 train_data, validation_data = train_validation_data.random_split(0.5, seed=1)
-
-from regression import get_residual_sum_of_squares as rss
 
 for degree in range(1,15+1):
     poldata = polynomial_sframe(train_data['sqft_living'], degree)
 
     feature = poldata.column_names()
-    print feature
+    print "Feature of model with degree[", degree, "] is: ", feature
 
     poldata['price'] = train_data['price']
+
     model = graphlab.linear_regression.create(poldata, target = 'price', features = feature, verbose = False, validation_set = None)
+
+    print "Coefficients of model with degree[", degree, "] is: ",
 
     model.get("coefficients")
 
     valdata = polynomial_sframe(validation_data['sqft_living'], degree)
+
     valdata['price'] = validation_data['price']
 
-    print 'RSS', rss(model, valdata, valdata['price'])
+    print 'RSS of model with degree[', degree, "]degree is: ", rss(model, valdata, valdata['price'])
