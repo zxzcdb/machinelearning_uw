894c03038369dd4224d9220cf532d7d2004b8648
Xingzhou Zhu <tianxiashilaofude@gmail.com>
2016-10-08 22:04:03 +0800

    Update decision_tree.py

--- classification/week3/decision_tree.py
+++ classification/week3/decision_tree.py
@@ -1,1 +1,92 @@
+import graphlab
 
+loans = graphlab.SFrame('lending-club-data.gl/')
+
+# safe_loans =  1 => safe
+# safe_loans = -1 => risky
+loans['safe_loans'] = loans['bad_loans'].apply(lambda x : +1 if x==0 else -1)
+loans = loans.remove_column('bad_loans')
+
+loans['safe_loans'].show(view = 'Categorical')
+
+features = ['grade',                     # grade of the loan
+            'sub_grade',                 # sub-grade of the loan
+            'short_emp',                 # one year or less of employment
+            'emp_length_num',            # number of years of employment
+            'home_ownership',            # home_ownership status: own, mortgage or rent
+            'dti',                       # debt to income ratio
+            'purpose',                   # the purpose of the loan
+            'term',                      # the term of the loan
+            'last_delinq_none',          # has borrower had a delinquincy
+            'last_major_derog_none',     # has borrower had 90 day or worse rating
+            'revol_util',                # percent of available credit being used
+            'total_rec_late_fee',        # total late fees received to day
+           ]
+
+target = 'safe_loans'                   # prediction target (y) (+1 means safe, -1 is risky)
+
+safe_loans_raw = loans[loans[target] == +1]
+risky_loans_raw = loans[loans[target] == -1]
+print "Number of safe loans  : %s" % len(safe_loans_raw)
+print "Number of risky loans : %s" % len(risky_loans_raw)
+
+# Since there are fewer risky loans than safe loans, find the ratio of the sizes
+# and use that percentage to undersample the safe loans.
+percentage = len(risky_loans_raw)/float(len(safe_loans_raw))
+
+risky_loans = risky_loans_raw
+safe_loans = safe_loans_raw.sample(percentage, seed=1)
+
+# Append the risky_loans with the downsampled version of safe_loans
+loans_data = risky_loans.append(safe_loans)
+
+# Extract the feature columns and target column
+loans = loans[features + [target]]
+
+train_data, validation_data = loans_data.random_split(.8, seed=1)
+
+decision_tree_model = graphlab.decision_tree_classifier.create(train_data, validation_set=None,
+                                target = target, features = features)
+
+small_model = graphlab.decision_tree_classifier.create(train_data, validation_set=None,
+                   target = target, features = features, max_depth = 2)
+
+small_model.show(view="Tree")
+
+validation_safe_loans = validation_data[validation_data[target] == 1]
+validation_risky_loans = validation_data[validation_data[target] == -1]
+
+sample_validation_data_risky = validation_risky_loans[0:2]
+sample_validation_data_safe = validation_safe_loans[0:2]
+
+sample_validation_data = sample_validation_data_safe.append(sample_validation_data_risky)
+sample_validation_data
+
+decision_tree_model.predict(sample_validation_data)
+
+decision_tree_model.predict(sample_validation_data, output_type='probability')
+
+small_model.predict(sample_validation_data, output_type='probability')
+
+small_model.show(view="Tree")
+
+print small_model.evaluate(train_data)['accuracy']
+print decision_tree_model.evaluate(train_data)['accuracy']
+
+big_model = graphlab.decision_tree_classifier.create(train_data, validation_set=None,
+                   target = target, features = features, max_depth = 10)
+
+print big_model.evaluate(train_data)['accuracy']
+print big_model.evaluate(validation_data)['accuracy']
+
+predictions = decision_tree_model.predict(validation_data)
+false_negative = 0
+false_positive = 0
+
+for i in range(len(predictions)):
+    if validation_data['safe_loans'][i] == 1 and predictions[i] == -1:
+        false_negative = false_negative + 1
+    elif validation_data['safe_loans'][i] == -1 and predictions[i] == 1:
+        false_positive = false_positive + 1
+
+total_cost = 10000 * false_negative + 20000 * false_positive
