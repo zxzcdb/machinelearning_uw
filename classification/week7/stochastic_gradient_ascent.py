e4d9bb3446a3394a4f68b4d6fbe38e87f3bdaf0a
Xingzhou Zhu <tianxiashilaofude@gmail.com>
2016-11-06 23:48:55 +0800

    Update stochastic_gradient_ascent.py

--- classification/week7/stochastic_gradient_ascent.py
+++ classification/week7/stochastic_gradient_ascent.py
@@ -1,1 +1,189 @@
+import graphlab
+import json
+import numpy as np
+
+products = graphlab.SFrame('amazon_baby_subset.gl/')
+
+with open('important_words.json', 'r') as f:
+    important_words = json.load(f)
+important_words = [str(s) for s in important_words]
+
+def remove_punctuation(text):
+    import string
+    return text.translate(None, string.punctuation)
+
+products['review_clean'] = products['review'].apply(remove_punctuation)
+
+# Split out the words into individual columns
+for word in important_words:
+    products[word] = products['review_clean'].apply(lambda s : s.split().count(word))
+
+train_data, validation_data = products.random_split(.9, seed=1)
+
+print 'Training set  : %d data points' % len(train_data)
+print 'Validation set: %d data points' % len(validation_data)
+
+def get_numpy_data(data_sframe, features, label):
+    data_sframe['intercept'] = 1
+    features = ['intercept'] + features
+    features_sframe = data_sframe[features]
+    feature_matrix = features_sframe.to_numpy()
+    label_sarray = data_sframe[label]
+    label_array = label_sarray.to_numpy()
+    return(feature_matrix, label_array)
+
+feature_matrix_train, sentiment_train = get_numpy_data(train_data, important_words, 'sentiment')
+feature_matrix_valid, sentiment_valid = get_numpy_data(validation_data, important_words, 'sentiment')
+
+def predict_probability(feature_matrix, coefficients):
+    # Take dot product of feature_matrix and coefficients
+    score = np.dot(feature_matrix, coefficients)
+
+    # Compute P(y_i = +1 | x_i, w) using the link function
+    predictions = 1. / (1.+np.exp(-score))
+    return predictions
+
+def feature_derivative(errors, feature):
+
+    # Compute the dot product of errors and feature
+    ## YOUR CODE HERE
+    derivative = (errors * feature).sum()
+
+    return derivative
+
+def compute_avg_log_likelihood(feature_matrix, sentiment, coefficients):
+
+    indicator = (sentiment==+1)
+    scores = np.dot(feature_matrix, coefficients)
+    logexp = np.log(1. + np.exp(-scores))
+
+    # Simple check to prevent overflow
+    mask = np.isinf(logexp)
+    logexp[mask] = -scores[mask]
+
+    lp = np.sum((indicator-1)*scores - logexp)/len(feature_matrix)
+
+    return lp
+
+j = 1                        # Feature number
+i = 10                       # Data point number
+coefficients = np.zeros(194) # A point w at which we are computing the gradient.
+
+predictions = predict_probability(feature_matrix_train[i:i+1,:], coefficients)
+indicator = (sentiment_train[i:i+1]==+1)
+
+errors = indicator - predictions
+gradient_single_data_point = feature_derivative(errors, feature_matrix_train[i:i+1,j])
+print "Gradient single data point: %s" % gradient_single_data_point
+print "           --> Should print 0.0"
+
+j = 1                        # Feature number
+i = 10                       # Data point start
+B = 10                       # Mini-batch size
+coefficients = np.zeros(194) # A point w at which we are computing the gradient.
+
+predictions = predict_probability(feature_matrix_train[i:i+B,:], coefficients)
+indicator = (sentiment_train[i:i+B]==+1)
+
+errors = indicator - predictions
+gradient_mini_batch = feature_derivative(errors, feature_matrix_train[i:i+B,j])
+print "Gradient mini-batch data points: %s" % gradient_mini_batch
+print "                --> Should print 1.0"
+
+from math import sqrt
+def logistic_regression_SG(feature_matrix, sentiment, initial_coefficients, step_size, batch_size, max_iter):
+    log_likelihood_all = []
+
+    # make sure it's a numpy array
+    coefficients = np.array(initial_coefficients)
+    # set seed=1 to produce consistent results
+    np.random.seed(seed=1)
+    # Shuffle the data before starting
+    permutation = np.random.permutation(len(feature_matrix))
+    feature_matrix = feature_matrix[permutation,:]
+    sentiment = sentiment[permutation]
+
+    i = 0 # index of current batch
+    # Do a linear scan over data
+    for itr in xrange(max_iter):
+        # Predict P(y_i = +1|x_i,w) using your predict_probability() function
+        # Make sure to slice the i-th row of feature_matrix with [i:i+batch_size,:]
+        ### YOUR CODE HERE
+        predictions = predict_probability(feature_matrix[i:i+batch_size,:], coefficients)
+
+        # Compute indicator value for (y_i = +1)
+        # Make sure to slice the i-th entry with [i:i+batch_size]
+        ### YOUR CODE HERE
+        indicator = (sentiment[i:i+batch_size]==+1)
+
+        # Compute the errors as indicator - predictions
+        errors = indicator - predictions
+        for j in xrange(len(coefficients)): # loop over each coefficient
+            # Recall that feature_matrix[:,j] is the feature column associated with coefficients[j]
+            # Compute the derivative for coefficients[j] and save it to derivative.
+            # Make sure to slice the i-th row of feature_matrix with [i:i+batch_size,j]
+            ### YOUR CODE HERE
+            derivative = feature_derivative(errors, feature_matrix[i:i+batch_size,j])
+
+            # compute the product of the step size, the derivative, and the **normalization constant** (1./batch_size)
+            ### YOUR CODE HERE
+            coefficients[j] += 1./batch_size * step_size * derivative
+
+        # Checking whether log likelihood is increasing
+        # Print the log likelihood over the *current batch*
+        lp = compute_avg_log_likelihood(feature_matrix[i:i+batch_size,:], sentiment[i:i+batch_size],
+                                        coefficients)
+        log_likelihood_all.append(lp)
+        if itr <= 15 or (itr <= 1000 and itr % 100 == 0) or (itr <= 10000 and itr % 1000 == 0) \
+         or itr % 10000 == 0 or itr == max_iter-1:
+            data_size = len(feature_matrix)
+            print 'Iteration %*d: Average log likelihood (of data points in batch [%0*d:%0*d]) = %.8f' % \
+                (int(np.ceil(np.log10(max_iter))), itr, \
+                 int(np.ceil(np.log10(data_size))), i, \
+                 int(np.ceil(np.log10(data_size))), i+batch_size, lp)
+
+        # if we made a complete pass over data, shuffle and restart
+        i += batch_size
+        if i+batch_size > len(feature_matrix):
+            permutation = np.random.permutation(len(feature_matrix))
+            feature_matrix = feature_matrix[permutation,:]
+            sentiment = sentiment[permutation]
+            i = 0
+
+    # We return the list of log likelihoods for plotting purposes.
+    return coefficients, log_likelihood_all
+
+sample_feature_matrix = np.array([[1.,2.,-1.], [1.,0.,1.]])
+sample_sentiment = np.array([+1, -1])
+
+coefficients, log_likelihood = logistic_regression_SG(sample_feature_matrix, sample_sentiment, np.zeros(3),
+                                                  step_size=1., batch_size=2, max_iter=2)
+print '-------------------------------------------------------------------------------------'
+print 'Coefficients learned                 :', coefficients
+print 'Average log likelihood per-iteration :', log_likelihood
+if np.allclose(coefficients, np.array([-0.09755757,  0.68242552, -0.7799831]), atol=1e-3)\
+  and np.allclose(log_likelihood, np.array([-0.33774513108142956, -0.2345530939410341])):
+    # pass if elements match within 1e-3
+    print '-------------------------------------------------------------------------------------'
+    print 'Test passed!'
+else:
+    print '-------------------------------------------------------------------------------------'
+    print 'Test failed'
+
+coefficients, log_likelihood = logistic_regression_SG(feature_matrix_train, sentiment_train,
+                                        initial_coefficients=np.zeros(194),
+                                        step_size=5e-1, batch_size=1, max_iter=10)
+
+coefficients_batch, log_likelihood_batch = logistic_regression_SG(feature_matrix_train, sentiment_train,
+                                        initial_coefficients=np.zeros(194),
+                                        step_size=5e-1, batch_size=1, max_iter=200)
+
+step_size = 1e-1
+batch_size = 100
+num_passes = 10
+num_iterations = num_passes * int(len(feature_matrix_train)/batch_size)
+
+coefficients_sgd, log_likelihood_sgd = logistic_regression_SG(feature_matrix_train, sentiment_train,
+                                       initial_coefficients=np.zeros(194),
+                                       step_size=1e-1, batch_size=100, max_iter=num_iterations)
 
